dc0cab660c9858f2df6f1047ff0a6194906532bc
alpersarac <alper.sarac42@gmail.com>
2023-07-13 02:05:30 +0300

    Setting default for select option

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,8 @@ import { NgForm } from '@angular/forms';
 })
 export class AppComponent {
   @ViewChild('f') signupForm: NgForm;
+  defaultQuestion="teacher";
+
   suggestUserName() {
     const suggestedName = 'Superuser';
   }
