18a8f2b45958226c3b7913a934d897ee12a02992
alpersarac <alper.sarac42@gmail.com>
2023-07-13 02:20:18 +0300

    Two way binding on the form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,7 @@ import { NgForm } from '@angular/forms';
 export class AppComponent {
   @ViewChild('f') signupForm: NgForm;
   defaultQuestion="teacher";
+  answer='';
 
   suggestUserName() {
     const suggestedName = 'Superuser';
