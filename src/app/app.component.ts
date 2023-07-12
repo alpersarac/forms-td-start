1e54f87df2d11a27f8b3afd284bf79715d2d7c64
alpersarac <alper.sarac42@gmail.com>
2023-07-12 23:57:13 +0300

    setting method to pass ts component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -8,5 +9,8 @@ import { Component } from '@angular/core';
 export class AppComponent {
   suggestUserName() {
     const suggestedName = 'Superuser';
+  }
+  onSubmit(form:NgForm){
+
   }
 }
